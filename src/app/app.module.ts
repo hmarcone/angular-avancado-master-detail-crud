8442defc287a2cb1debdc5d80d2869d083c91cd6
Humberto Marcone <humberto.freitas@antt.gov.br>
2019-10-15 16:58:01 -0300

    project-improvements: project restructuring

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,29 +1,22 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { NgModule } from '@angular/core';
-import { HttpClientModule } from '@angular/common/http';
 
+import { CoreModule } from "./core/core.module";
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
-import { HttpClientInMemoryWebApiModule} from "angular-in-memory-web-api";
-import { InMemoryDatabase } from "./in-memory-database";
-import { ToastrModule } from 'ngx-toastr';
+//import { ToastrModule } from 'ngx-toastr';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    BrowserAnimationsModule,
-    AppRoutingModule,
-    HttpClientInMemoryWebApiModule.forRoot(InMemoryDatabase),
-    HttpClientModule,
-    ToastrModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent
+    ],
+    imports: [
+        CoreModule,
+        AppRoutingModule
+        //ToastrModule
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
 
 export class AppModule { }
